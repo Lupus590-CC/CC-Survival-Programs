86948bed93d6016014c0b28b49e0da68a63c6950
Lupus590 <lupussolitarius590@gmail.com>
2020-05-25 22:43:47 +0100

    faster

--- assets/computercraft/lua/rom/Programs/turtle/compactor.lua
+++ assets/computercraft/lua/rom/Programs/turtle/compactor.lua
@@ -4,6 +4,7 @@ local threeXThreeMode = true -- true for 3x3 crafts, false for 2x2
 local maxWaitTime = 120 -- when the output chest is fuil we wait a short amount
 -- of time, if it still is full then we wait longer. This value it the maximum
 -- amount of time to wait before checking again
+local inputChestSide = "bottom" -- optional, speeds up pulling items. Doesn't have to be a chest, but must be an inventory
 
 local threeXThreeSlots = {1,2,3,5,6,7,9,10,11}
 local twoXTwoSlots = {1,2,5,6}
@@ -14,6 +15,8 @@ if threeXThreeMode then
 else
   slots = twoXTwoSlots
 end
+
+local inputChest = peripheral.wrap(inputChestSide)
 
 while true do
   local sleepTime = 0
@@ -31,13 +34,27 @@ while true do
     print("Continuing")
   end
 
-  -- TODO: make item pulling faster, pull more and distibute?
-  -- wrap the chest to find out how many items are in it to
-  -- calculate how many to pull at a time
+  local amountToPull = 1
+  if inputChest then
+    repeat
+      local total = 0
+      for _, item in pairs(inputChest.list()) do
+        total = total + item.count
+      end
+      if threeXThreeMode then
+        amountToPull = math.floor(total/9)
+      else
+        amountToPull = math.floor(total/4)
+      end
+      amountToPull = math.min(amountToPull, 64)
+    until amountToPull > 0
+  end
+
   for _, slot in ipairs(slots) do
-    if turtle.getItemCount(slot) == 0 then
+    local currentCount = turtle.getItemCount(slot)
+    if currentCount < amountToPull then
       turtle.select(slot)
-      while not suckFunc(1) do end
+      while not suckFunc(amountToPull - currentCount) do end
     end
   end
 
