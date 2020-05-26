c353e7aea9dc1e47a2f7bc35deac32df5633de45
Lupus590 <lupussolitarius590@gmail.com>
2020-05-26 19:33:37 +0100

    parallel, eject and dig at the same time

--- assets/computercraft/lua/rom/Programs/turtle/cobbleGen.lua
+++ assets/computercraft/lua/rom/Programs/turtle/cobbleGen.lua
@@ -1,8 +1,9 @@
 local dropFunc = turtle.dropUp -- set to nil to disable auto eject
+local maxWaitTime = 120
 
 local function allFull()
   for i = 1, 16 do
-    if turtle.getItemSpace(i) > 0 then
+    if turtle.getItemCount(i) == 0 or turtle.getItemSpace(i) < 0 then
       return false
     end
   end
@@ -10,32 +11,36 @@ local function allFull()
 end
 
 local function emptyAll()
-  for i = 1, 16 do
-    local sleepTime = 0
-    local _, y = term.getCursorPos()
-    turtle.select(i)
-    while turtle.getItemSpace(i) > 0 and (not dropFunc())
-    and turtle.getItemSpace(i) > 0 do -- check twice as drop may not drop everything
-      term.setCursorPos(1, y)
-      sleepTime = math.min(sleepTime + math.floor(sleepTime/2) +1, maxWaitTime)
-      write("Output full, waiting "..tostring(sleepTime).." seconds.")
-      os.sleep(sleepTime)
-    end
-    if sleepTime > 0 then
-      print("Continuing")
+  while dropFunc do
+    for i = 1, 16 do
+      local sleepTime = 0
+      local _, y = term.getCursorPos()
+      turtle.select(i)
+      while turtle.getItemCount(i) > 0 and (not dropFunc())
+      and turtle.getItemCount(i) > 0 do -- check twice as drop may not drop everything
+        term.setCursorPos(1, y)
+        sleepTime = math.min(sleepTime + math.floor(sleepTime/2) +1, maxWaitTime)
+        write("Output full, waiting "..tostring(sleepTime).." seconds.")
+        os.sleep(sleepTime)
+      end
+      if sleepTime > 0 then
+        print("Continuing output")
+      end
     end
   end
 end
 
-while true do
-  while allFull() do
-    if dropFunc then
-      emptyAll()
+local function dig()
+  while true do
+    if not allFull() then
+      turtle.dig()
     else
-      print("Paused due to full inventory, remove blocks to resume.")
+      print("Digging paused due to full inventory, remove blocks to resume.")
       os.pullEvent("turtle_inventory")
-      print("Detected inventory change, attemting to resume.")
+      print("Detected inventory change, attemting to resume digging.")
     end
   end
-  turtle.dig()
 end
+
+
+parallel.waitForAll(dig, emptyAll)
