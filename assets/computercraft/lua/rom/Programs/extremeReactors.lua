4b63d0ebd75e9e1b2afd0c8e15d3bad1c89354bb
Lupus590 <lupussolitarius590@gmail.com>
2020-08-06 01:31:28 +0100

    I broke things, this fixes them

--- assets/computercraft/lua/rom/Programs/extremeReactors.lua
+++ assets/computercraft/lua/rom/Programs/extremeReactors.lua
@@ -1,21 +1,18 @@
 local reactorName = "BigReactors-Reactor_0"
 local overrideSide = "top" -- redstone signal disables the computers modifying the reactor
 local sleepTime = 1
-local rodInsertionAdditive = -0.5 -- a fudge value which adjusts the insertion amount ignoreing the energy level.
-    -- Negative values mean that the energy buffer be allowed to continue filling after matching the consumption rate.
-    -- Positive means that the energy buffer will never begin to fill as it will always be producing too little energy to match the consumption rate
 
 local reactor = peripheral.wrap(reactorName) or error("couldn't locate reactor with name/side "..reactorName)
 local override = false
 
 local function maintainenceLoop()
     while true do
-        if not override then            
+        if not override then
             reactor.setActive(true)
             local energyStored = reactor.getEnergyStored()
             local energyCapacity = reactor.getEnergyCapacity()
             local energyFilledPercentage = (energyStored / energyCapacity) * 100
-            local rodLevelToSet = math.max(math.min(energyFilledPercentage + rodInsertionAdditive, 0), 100)
+            local rodLevelToSet = math.min(math.max(energyFilledPercentage, 0), 100)
             reactor.setAllControlRodLevels(rodLevelToSet)
             sleep(sleepTime)
         else
