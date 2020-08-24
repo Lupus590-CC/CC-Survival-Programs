17a66ea7942e7fd4015f69290ed05f36d9868e9a
Lupus590 <lupussolitarius590@gmail.com>
2020-08-24 18:55:19 +0100

    preliminary active cool

--- assets/computercraft/lua/rom/Programs/extremeReactors.lua
+++ assets/computercraft/lua/rom/Programs/extremeReactors.lua
@@ -1,8 +1,11 @@
-local reactorName = "BigReactors-Reactor_0"
+local reactorName = "BigReactors-Reactor_1"
+local turbineName = "BigReactors-Turbine_3"
 local overrideSide = "top" -- redstone signal disables the computers modifying the reactor
 local sleepTime = 1
 
-local reactor = peripheral.wrap(reactorName) or error("couldn't locate reactor with name/side "..reactorName)
+local BEST_TURBINE_SPEED = 1800
+local reactor = peripheral.wrap(reactorName) or error("couldn't locate reactor with name/side "..reactorName, 0)
+local turbine = reactor.isActivelyCooled() and (peripheral.wrap(turbineName) or error("couldn't locate turbine with name/side "..turbineName, 0)) or nil
 local override = false
 
 local function bufferOpimiser(x)
@@ -13,15 +16,49 @@ local function bufferOpimiser(x)
     return y
 end
 
-local function maintainenceLoop()
-    while true do
-        if not override then
-            reactor.setActive(true)
+local function passivelyCooled()
     local energyStored = reactor.getEnergyStored()
     local energyCapacity = reactor.getEnergyCapacity()
     local energyFilledPercentage = (energyStored / energyCapacity) * 100
     local rodLevelToSet = bufferOpimiser(energyFilledPercentage)
     reactor.setAllControlRodLevels(rodLevelToSet)
+end
+
+local function activelyCooled()
+    local energyStored = turbine.getEnergyStored()
+    local energyCapacity = turbine.getEnergyCapacity()    
+    local energyFilledPercentage = (energyStored / energyCapacity) * 100
+    turbine.setInductorEngaged(energyFilledPercentage < 75)
+
+    local turbineSpeed = turbine.getRotorSpeed()
+    if turbineSpeed > BEST_TURBINE_SPEED then
+        turbine.setFluidFlowRateMax(turbine.getFluidFlowRateMax()-1)
+    else
+        turbine.setFluidFlowRateMax(turbine.getFluidFlowRateMax()+1)
+    end
+
+    local steamStored = reactor.getHotFluidAmount()
+    local steamCapacity = reactor.getHotFluidAmountMax()
+    local steamFilledPercentage = (steamStored / steamCapacity) * 100
+    local rodLevelToSet = bufferOpimiser(steamFilledPercentage)
+    reactor.setAllControlRodLevels(rodLevelToSet)
+end
+
+local function maintanenceLoop()
+    while true do
+        if not override then
+            reactor.setActive(true)
+
+            if reactor.isActivelyCooled() then
+                if not turbine then error("turbine not set up, did you change the multiblock?", 0) end
+                turbine.setActive(true)
+                turbine.setVentAll()
+                activelyCooled()
+            else
+                passivelyCooled()
+            end
+
+
             sleep(sleepTime)
         else
             os.pullEvent("redstone")
