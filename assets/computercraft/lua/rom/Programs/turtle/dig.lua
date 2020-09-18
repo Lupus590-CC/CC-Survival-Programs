e854555c4dbe2d180f0c574f2b5164bdcbb24a10
Lupus590 <lupussolitarius590@gmail.com>
2020-09-18 23:08:02 +0100

    add dig

---
+++ assets/computercraft/lua/rom/Programs/turtle/dig.lua
@@ -0,0 +1,14 @@
+local dir = ...
+if dir and type(dir) ~= "string" then
+  error("bad arg")
+end
+dir = dir:lower()
+if dir == "d" or dir == "down" then
+  turtle.digDown()
+elseif dir == "u" or dir == "up" then
+  turtle.digUp()
+elseif dir == nil or dir == "f" or dir == "forwards" or dir == "forward" then
+  turtle.dig()
+else
+  error("bad arg")
+end
