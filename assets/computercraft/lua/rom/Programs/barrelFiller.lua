cea1fdd445f2c45f53eec815335b8313d9c6292e
Lupus590 <lupussolitarius590@gmail.com>
2020-09-18 22:44:31 +0100

    add barrelFiller

---
+++ assets/computercraft/lua/rom/Programs/barrelFiller.lua
@@ -0,0 +1,39 @@
+--config
+local SOURCE_NAME = "minecraft:chest_38"
+local DESTINATION_NAME = "minecraft:chest_39"
+local SLEEP_SECONDS = 1
+
+--code
+local function addPeripheralName(peripheralName, wrappedPeripheral)
+    wrappedPeripheral.PERIPHERAL_NAME = peripheralName
+    return wrappedPeripheral
+end
+
+local barrelTable = table.pack(peripheral.find("minecraft:block_barrel", addPeripheralName))
+local inputChest = peripheral.wrap(SOURCE_NAME)
+local outputChest = peripheral.wrap(DESTINATION_NAME)
+
+
+local function moveToBarrel()
+    while true do
+        for _, barrel in ipairs(barrelTable) do
+            for slot = 1, inputChest.size() do
+                while inputChest.pushItems(barrel.PERIPHERAL_NAME, slot) > 0 do end
+            end
+        end
+        sleep(SLEEP_SECONDS)
+    end
+end
+
+local function moveFromBarrel()
+    while true do
+        for _, barrel in ipairs(barrelTable) do
+            outputChest.pullItems(barrel.PERIPHERAL_NAME, 1)
+        end
+        sleep(SLEEP_SECONDS)
+    end
+end
+
+while true do
+    parallel.waitForAny(moveToBarrel,moveFromBarrel)
+end
