4ad5ba4ae2f91ffff01119b693452ac582e1176e
Lupus590 <lupussolitarius590@gmail.com>
2020-10-06 12:15:26 +0100

    add single treefarm smelter

---
+++ assets/computercraft/lua/rom/Programs/singleTreeFarmCompanionSmelter.lua
@@ -0,0 +1,100 @@
+local inputChests = {"minecraft:chest_41", "minecraft:chest_45"}
+local refuelChests = {"minecraft:chest_40", "minecraft:chest_46"}
+local outputChest = "minecraft:chest_42"
+local proportionOfWoodToSmelt = 1/2
+local sleepTime = 120
+local compactorInputChest = "minecraft:chest_44"
+local compactorOutputChest = "minecraft:chest_43"
+
+local electricFurnaceName = "minecraft:gc electric furnace"
+local electricFurnaceInputSlot = 2
+local electricFurnaceOutputSlots = {3,4}
+
+local function addPeripheralName(peripheralName, wrappedPeripheral)
+    wrappedPeripheral.PERIPHERAL_NAME = peripheralName
+    return wrappedPeripheral
+end
+
+local peripheralName = compactorInputChest
+compactorInputChest = peripheral.wrap(compactorInputChest)
+compactorInputChest.PERIPHERAL_NAME = peripheralName
+peripheralName = compactorOutputChest
+compactorOutputChest = peripheral.wrap(compactorOutputChest)
+compactorOutputChest.PERIPHERAL_NAME = peripheralName
+peripheralName = outputChest
+outputChest = peripheral.wrap(outputChest)
+outputChest.PERIPHERAL_NAME = peripheralName
+
+for k, v in ipairs(inputChests) do
+    inputChests[k] = peripheral.wrap(v)
+    inputChests[k].PERIPHERAL_NAME = v
+end
+for k, v in ipairs(refuelChests) do
+    refuelChests[k] = peripheral.wrap(v)
+    refuelChests[k].PERIPHERAL_NAME = v
+end
+
+local electricFurnaces = table.pack(peripheral.find(electricFurnaceName, addPeripheralName))
+
+
+local function splitStacksToTarget(stackSize, destinationCount)
+    return math.floor(stackSize/destinationCount)
+end
+
+local function compactSlots(chest)
+    for slot in pairs(chest.list()) do
+        chest.pushItems(chest.PERIPHERAL_NAME, slot)
+    end
+end
+
+local function moveFuelToChests()
+    compactSlots(compactorOutputChest) -- TODO: optimise
+    local item = compactorOutputChest.getItemMeta(1)
+    if not item then return end
+    local limit = splitStacksToTarget(item.count, #refuelChests)
+    if limit < 1 then return end
+    for _, refuelChest in ipairs(refuelChests) do
+        compactSlots(compactorOutputChest) -- TODO: optimise
+        compactorOutputChest.pushItems(refuelChest.PERIPHERAL_NAME, 1, limit)
+    end
+end
+
+local function moveToSmelters(chest, slot, item)
+    local amountToSmelt = math.ceil(item.count * proportionOfWoodToSmelt)
+    local limit = math.ceil(amountToSmelt / #electricFurnaces)
+    
+    for i, furnace in ipairs(electricFurnaces) do
+        if i > amountToSmelt then return end
+        
+        chest.pushItems(furnace.PERIPHERAL_NAME, slot, limit, electricFurnaceInputSlot)
+    end
+end
+
+local function emptyInputChests()
+    for _, inputChest in ipairs(inputChests) do
+        for slot in pairs(inputChest.list()) do
+            local item = inputChest.getItemMeta(slot)
+            if item then
+                if item.name == "minecraft:log" then
+                    moveToSmelters(inputChest, slot, item)
+                end
+                inputChest.pushItems(outputChest.PERIPHERAL_NAME, slot)
+            end
+        end
+    end
+end
+
+local function loadCompactor()
+    for _, furnace in ipairs(electricFurnaces) do
+        for _, outputSlot in ipairs(electricFurnaceOutputSlots) do
+            furnace.pushItems(compactorInputChest.PERIPHERAL_NAME, outputSlot)
+        end
+    end
+end
+
+while true do
+    moveFuelToChests()
+    loadCompactor()
+    emptyInputChests()
+    sleep(sleepTime)
+end
