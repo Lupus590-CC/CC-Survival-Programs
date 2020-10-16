962825b3ed3bfd095b0dc60ee4d3b9a8a692c9cf
Lupus590 <lupussolitarius590@gmail.com>
2020-10-16 23:56:49 +0100

    add shell completion
    this is really not intuitive

--- assets/computercraft/lua/rom/Programs/turtle/dig.lua
+++ assets/computercraft/lua/rom/Programs/turtle/dig.lua
@@ -1,3 +1,10 @@
+
+local completion = require("cc.shell.completion")
+
+shell.setCompletionFunction(shell.getRunningProgram(), completion.build(
+    { completion.choice, { "down", "up", "forward" } }
+))
+
 local dir = ...
 if dir and type(dir) ~= "string" then
   error("bad arg", 0)
@@ -9,6 +16,8 @@ elseif dir == "u" or dir == "up" then
   turtle.digUp()
 elseif dir == nil or dir == "f" or dir == "forwards" or dir == "forward" then
   turtle.dig()
+elseif dir == "addshellcomplete" then
+  return
 else
   error("bad arg", 0)
 end
