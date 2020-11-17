0491caf7b75987a8aa4148319a9d198e1f2b751c
Lupus590 <lupussolitarius590@gmail.com>
2020-11-17 09:52:37 +0000

    fuel and block intelligence
    we now wait for more blocks and/or fuel if we need them

--- assets/computercraft/lua/rom/Programs/turtle/bridge.lua
+++ assets/computercraft/lua/rom/Programs/turtle/bridge.lua
@@ -1,22 +1,60 @@
-
+local cantMoveMaxWait = 10
 local TURTLE_SLOT_COUNT = 16
 
-local function selectSlotWithBlock() -- TODO: placeable inteligence
-    if turtle.getItemCount() > 0 then
+local function selectNextSlot()
+    local slot = turtle.getSelectedSlot()
+    local slotToSelect = slot +1
+    if slotToSelect > TURTLE_SLOT_COUNT then
+        slotToSelect = 1
+    end
+    turtle.select(slotToSelect)
+end
+
+local skipped = {}
+local function selectSlotWithBlock(skipCurrent)
+    if  (not skipCurrent) and turtle.getItemCount() > 0 then
         return
     end
+    if skipCurrent then
+        skipped[turtle.getSelectedSlot()] = true
+    end
     local startSlot = turtle.getSelectedSlot()
-    while turtle.getItemCount() == 0 do
-        local slot = turtle.getSelectedSlot()
-        local slotToSelect = slot +1
-        if slotToSelect > TURTLE_SLOT_COUNT then
-            slotToSelect = 1
-        end
-        turtle.select(slotToSelect)
-        if slotToSelect == startSlot then
+    repeat
+        selectNextSlot()
+        if turtle.getSelectedSlot() == startSlot then
             print("Out of blocks, add more to continue.")
             os.pullEvent("turtle_inventory")
+            skipped = {}
         end
+    until (not skipped[turtle.getSelectedSlot()]) and turtle.getItemCount() > 0
+end
+
+local function refuel()
+    local oldSlelectedSlot = turtle.getSelectedSlot()
+    repeat
+        selectNextSlot()
+        if turtle.getSelectedSlot() == oldSlelectedSlot then
+            print("Out of fuel, add more to continue.")
+            os.pullEvent("turtle_inventory")
+            skipped = {}
+        end
+    until turtle.refuel()
+    turtle.select(oldSlelectedSlot)
+end
+
+local function progressivlyLongerWaitFor(callback, maxWaitTime)
+    if type(callback) ~= "function" then
+        error("bad arg[1], expected function got "..type(callback), 2)
+    end
+    if maxWaitTime ~= nil and type(maxWaitTime) ~= "number" then
+        error("bad arg[2], expected number or nil got "..type(maxWaitTime), 2)
+    end
+
+    maxWaitTime = maxWaitTime or math.huge -- math.huge in inf, this might cause issues if we actually wait for ages
+    local sleepTime = 0
+    while not callback() do
+        sleepTime = math.min(sleepTime + math.floor(sleepTime/2) +1, maxWaitTime)
+        os.sleep(sleepTime)
     end
 end
 
@@ -30,8 +68,17 @@ while true do
     if distance then
         for i = 1, distance do
             selectSlotWithBlock()
-            turtle.placeDown()
-            turtle.forward() -- TODO: fuel inteligence
+            if not turtle.placeDown() then
+                selectSlotWithBlock(true)
+            end
+            while not turtle.forward() do
+                if turtle.getFuelLevel() == 0 then
+                    refuel()
+                else
+                    printError("Can't move, please remove obstuctions to continue.")
+                    progressivlyLongerWaitFor(function() return not turtle.detect() end, cantMoveMaxWait)
+                end
+            end
         end
     end
     local input
