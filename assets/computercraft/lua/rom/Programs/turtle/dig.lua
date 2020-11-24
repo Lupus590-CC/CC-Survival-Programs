fd84cadf8c47677657322452bc47b0cf653c03bb
Lupus590 <lupussolitarius590@gmail.com>
2020-11-24 15:06:48 +0000

    usage printout

--- assets/computercraft/lua/rom/Programs/turtle/dig.lua
+++ assets/computercraft/lua/rom/Programs/turtle/dig.lua
@@ -7,19 +7,32 @@ if not shell.complete(shell.getRunningProgram().." ") then
   ))
 end
 
-local dir = ...
-if dir and type(dir) ~= "string" then
-  error("bad arg", 0)
+local function printUsage()
+  local programName = arg[0] or fs.getName(shell.getRunningProgram())
+  print("Usage: " .. programName .. " down")
+  print("Usage: " .. programName .. " up")
+  print("Usage: " .. programName .. " forward")
+  print("Usage: " .. programName .. " addShellComplete")
 end
-dir = dir and dir:lower() or nil
+
+local arg = arg or table.pack(...)
+if #arg ~= 1 then
+  printUsage()
+  return
+end
+
+
+
+local dir = arg[1] and arg[1]:lower() or nil
 if dir == "d" or dir == "down" then
   turtle.digDown()
 elseif dir == "u" or dir == "up" then
   turtle.digUp()
-elseif dir == nil or dir == "f" or dir == "forwards" or dir == "forward" then
+elseif dir == nil or dir == "" or dir == "f" or dir == "forwards" or dir == "forward" then
   turtle.dig()
 elseif dir == "addshellcomplete" then
   return
 else
-  error("bad arg", 0)
+  printUsage()
+  return
 end
