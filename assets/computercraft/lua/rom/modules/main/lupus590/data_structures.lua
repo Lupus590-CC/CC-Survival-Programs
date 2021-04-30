005b8baaeaa7c3e6ff7d66cbffccc7207f914556
Lupus590 <lupussolitarius590@gmail.com>
2021-04-30 22:42:39 +0100

    Add queue, fix bug

--- assets/computercraft/lua/rom/modules/main/lupus590/data_structures.lua
+++ assets/computercraft/lua/rom/modules/main/lupus590/data_structures.lua
@@ -2,24 +2,47 @@ local function newStack()
     local stack = {_backingTable = {n=0}}
 
     function stack.isEmpty()
-        return _backingTable.n == 0
+        return stack._backingTable.n == 0
     end
 
     function stack.push(value)
-        stack._backingTable.n = _backingTable.n + 1
-        stack._backingTable[_backingTable.n] = value
+        stack._backingTable.n = stack._backingTable.n + 1
+        stack._backingTable[stack._backingTable.n] = value
     end
 
     function stack.pop()
         if stack.isEmpty() then
             error("Stack is empty, can't pop an empty stack.", 2)
         end
-        local value = stack._backingTable[_backingTable.n]
-        stack._backingTable.n = _backingTable.n - 1
+        local value = stack._backingTable[stack._backingTable.n]
+        stack._backingTable.n = stack._backingTable.n - 1
+        return value
+    end
+end
+
+local function newQueue()
+    local queue = {_backingTable = {n=0}}
+
+    function queue.isEmpty()
+        return queue._backingTable.n == 0
+    end
+
+    function queue.enqueue(value)
+        queue._backingTable.n = queue._backingTable.n + 1
+        queue._backingTable[queue._backingTable.n] = value
+    end
+
+    function stack.dequeue()
+        if queue.isEmpty() then
+            error("Queue is empty, can't dequeue an empty queue.", 2)
+        end
+        local value = queue._backingTable[queue._backingTable.n]
+        queue._backingTable.n = queue._backingTable.n - 1
         return value
     end
 end
 
 return {
     newStack = newStack,
+    newQueue = newQueue,
 }
