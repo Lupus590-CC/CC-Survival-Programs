0bab8a3b2f3367cb1ee2909bfdc0738797810804
Lupus590 <lupussolitarius590@gmail.com>
2021-05-01 00:24:20 +0100

    fix stack, queue now works but never cleans the backing table

--- assets/computercraft/lua/rom/modules/main/lupus590/data_structures.lua
+++ assets/computercraft/lua/rom/modules/main/lupus590/data_structures.lua
@@ -18,31 +18,46 @@ local function newStack()
         stack._backingTable.n = stack._backingTable.n - 1
         return value
     end
+    return stack
 end
 
-local function newQueue() -- TODO: make this a queue instead of a stack
-    local queue = {_backingTable = {n=0}}
+local function newQueue()
+  local queue = {_backingTable = {head=1, tail = 0}, _maxHeadDrift = 10}
 
-    function queue.isEmpty()
-        return queue._backingTable.n == 0
-    end
+  function queue.isEmpty()
+      return (queue._backingTable.tail - queue._backingTable.head) == 0
+  end
 
-    function queue.enqueue(value)
-        queue._backingTable.n = queue._backingTable.n + 1
-        queue._backingTable[queue._backingTable.n] = value
-    end
+  function queue._compact() -- TODO: fix
+      if queue.isEmpty() or queue._backingTable.head < queue._maxHeadDrift then return end
+      local newPos = 1
+      local backingTable = queue._backingTable
+      for pos = backingTable.head, backingTable.tail do
+          backingTable[newPos] = backingTable[pos]
+          newPos = newPos + 1
+      end
+      backingTable.head = 0
+      backingTable.tail = newPos
+  end
 
-    function stack.dequeue()
-        if queue.isEmpty() then
-            error("Queue is empty, can't dequeue an empty queue.", 2)
-        end
-        local value = queue._backingTable[queue._backingTable.n]
-        queue._backingTable.n = queue._backingTable.n - 1
-        return value
-    end
+  function queue.enqueue(value)
+      queue._backingTable.tail = queue._backingTable.tail + 1
+      queue._backingTable[queue._backingTable.tail] = value
+  end
+
+  function queue.dequeue()
+      if queue.isEmpty() then
+          error("Queue is empty, can't dequeue an empty queue.", 2)
+      end
+      --queue._compact() -- TODO: restore
+      local value = queue._backingTable[queue._backingTable.head]
+      queue._backingTable.head = queue._backingTable.head + 1
+      return value
+  end
+  return queue
 end
 
 return {
     newStack = newStack,
-    -- newQueue = newQueue, -- TODO: put this back once it's working
+    newQueue = newQueue,
 }
