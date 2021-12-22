eb480b8bace40a82b81669759b3ed67ccedd38de
Lupus590 <lupussolitarius590@gmail.com>
2021-12-22 17:09:06 +0000

    shorter notation

--- assets/computercraft/lua/rom/modules/main/lupus590/duration.lua
+++ assets/computercraft/lua/rom/modules/main/lupus590/duration.lua
@@ -1,14 +1,24 @@
 local duration= {
-  second = 1,
-  seconds = 1,
-  minute = 60,
-  minutes = 60,
-  hour = 3600,
-  hours = 3600,
-  day = 86400,
-  days = 86400
-  -- if you need longer than this then define it yourself
-  -- I highly doubt that you're going to have a day long timer anyways
+	second = 1,
+	seconds = 1,
+	sec = 1,
+	s = 1,
+	
+	minute = 60,
+	minutes = 60,
+	min = 60,
+	m = 60,
+	
+	hour = 3600,
+	hours = 3600,
+	h = 3600,
+	
+	day = 86400,
+	days = 86400,
+	d = 86400,
+	-- if you need longer than this then define it yourself
+	-- I highly doubt that you're going to have a day long timer anyways
 }
+
 
 return duration
