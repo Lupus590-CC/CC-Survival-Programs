f6c8d09da7ae943d9f66ff90e2d91b6e1cd300f0
Lupus590 <lupussolitarius590@gmail.com>
2021-12-22 17:04:27 +0000

    Create duration.lua

---
+++ assets/computercraft/lua/rom/modules/main/lupus590/duration.lua
@@ -0,0 +1,14 @@
+local duration= {
+  second = 1,
+  seconds = 1,
+  minute = 60,
+  minutes = 60,
+  hour = 3600,
+  hours = 3600,
+  day = 86400,
+  days = 86400
+  -- if you need longer than this then define it yourself
+  -- I highly doubt that you're going to have a day long timer anyways
+}
+
+return duration
